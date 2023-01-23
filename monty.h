bd8a7fec1a08e50ae9d1e79fda0b3a82f82818ea
PabloJermin <jerminamartey@gmail.com>
2023-01-23 06:05:46 -0800

    monty h file

--- monty.h
+++ monty.h
@@ -1,8 +1,6 @@
-fndef TADEM_H
+#ifndef TADEM_H
 
 #define TADEM_H
-
-
 
 #include <stdio.h>
 
@@ -21,32 +19,23 @@ fndef TADEM_H
 
 
 /**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
  *
- * * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
  *
- * * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
  *
- * * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ * Description: doubly linked list node structure
  *
- * * @next: points to the next element of the stack (or queue)
- *
- * *
- *
- * * Description: doubly linked list node structure
- *
- * * for stack, queues, LIFO, FIFO
- *
- * */
+ * for stack, queues, LIFO, FIFO
+ */
 
 typedef struct stack_s
-
 {
-
-		int n;
-
-			struct stack_s *prev;
-
-				struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 
 } stack_t;
 
@@ -55,29 +44,19 @@ typedef struct stack_s
 
 
 /**
+ * struct instruction_s - opcode and its function
  *
- * * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ * Description: opcode and its function
  *
- * * @opcode: the opcode
- *
- * * @f: function to handle the opcode
- *
- * *
- *
- * * Description: opcode and its function
- *
- * * for stack, queues, LIFO, FIFO
- *
- * */
+ * for stack, queues, LIFO, FIFO
+ */
 
 typedef struct instruction_s
-
 {
-
-		char *opcode;
-
-			void (*f)(stack_t **stack, unsigned int line_number);
-
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 
@@ -85,36 +64,26 @@ typedef struct instruction_s
 
 
 /**
+ * struct bus_s - variables -args, file, line content
  *
- * * struct bus_s - variables -args, file, line content
+ * @arg: value
  *
- * * @arg: value
+ * @file: pointer to monty file
  *
- * * @file: pointer to monty file
+ * @content: line content
  *
- * * @content: line content
- *
- * * @lifi: flag change stack <-> queue
- *
- * *
- *
- * * Description: carries values through the program
- *
- * */
+ * @lifi: flag change stack <-> queue
+ * Description: carries values through the program
+ */
 
 typedef struct bus_s
 
 {
-
-		char *arg;
-
-			FILE *file;
-
-				char *content;
-
-					int lifi;
-
-}  bus_t;
+	char *arg;
+	FILE *file;
+	char *content;
+	int lifi;
+}bus_t;
 
 extern bus_t bus;
 
